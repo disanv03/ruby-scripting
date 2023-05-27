4eb281bfa0d44898ef31914e6e7ea2288a4eb3b1
disanv03 <heddisanv@gmail.com>
2023-05-27 14:54:54 +0200

    some add

---
+++ basics/class/emprunt.rb
@@ -0,0 +1,67 @@
+require 'date'
+
+class Emprunt
+  attr_accessor :montant, :duree, :date_debut
+
+  MOIS = %w[Jan Fév Mar Avr Mai Jun Jul Aoû Sep Oct Nov Déc] # Noms des mois en français
+
+  def initialize(montant, duree, date_debut)
+    @montant = montant
+    @duree = duree * 12 # Convertir la durée en années en mois
+    @date_debut = Date.strptime(date_debut, '%d/%m/%Y') # Convertir la chaîne en date
+  end
+
+  def paiement_mensuel
+    @montant / @duree
+  end
+
+  def echeancier
+    solde_restant = @montant
+    date_paiement = @date_debut
+
+    File.open('echeancier.txt', 'w') do |f|
+        @duree.times do |i|
+            paiement = paiement_mensuel
+            solde_restant -= paiement
+            # format for cmdline display
+            date_format = "#{date_paiement.day} #{MOIS[date_paiement.month-1]} #{date_paiement.year}"
+            paiement_format = "#{paiement.round(2)}€"
+            solde_restant_format = "#{solde_restant.round(2)}€"
+            f.puts "#{date_format.center(18)} #{paiement_format.center(18)} #{solde_restant_format.center(14)}"
+            date_paiement = date_paiement >> 1 # ajouter un mois à la date
+        end
+    end
+  end
+end
+
+
+puts "Entrer le montant du prêt:"
+montant = gets.chomp.to_f
+puts "Entrer la durée du prêt (en années):"
+duree = gets.chomp.to_i
+puts "Entrer la date de début du prêt (jj/mm/aaaa):"
+date_debut = gets.chomp
+
+emprunt = Emprunt.new(montant, duree, date_debut)
+
+STDOUT.reopen(File.open('echeancier.txt', 'w'))
+
+puts ""
+puts "***************************************************"
+puts "Date: #{date_debut}"
+puts "Montant du prêt: #{montant}"
+puts "Durée du prêt: #{duree} ans"
+puts "Paiement mensuel: #{emprunt.paiement_mensuel.round(2)}€"
+puts "***************************************************"
+puts "Date du paiement - Paiement mensuel - Solde restant"
+puts "***************************************************"
+emprunt.echeancier
+puts "***************************************************"
+puts "Fait le:              à              ;"
+puts ""
+puts "Signatures:"
+puts ""
+puts ""
+puts ""
+puts ""
+puts "***************************************************"
