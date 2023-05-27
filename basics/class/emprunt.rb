3ae8b4072141ddf66842fa379fe601334dd1511a
disanv03 <heddisanv@gmail.com>
2023-05-27 15:01:59 +0200

    echeancier

--- basics/class/emprunt.rb
+++ basics/class/emprunt.rb
@@ -15,24 +15,47 @@ class Emprunt
     @montant / @duree
   end
 
+  def generate_output
+    output = ""
+    output += "***************************************************\n"
+    output += "Date: #{@date_debut}\n"
+    output += "Montant du prêt: #{@montant}\n"
+    output += "Durée du prêt: #{@duree/12} ans\n"
+    output += "Paiement mensuel: #{paiement_mensuel.round(2)}€\n"
+    output += "***************************************************\n"
+    output += "Date du paiement - Paiement mensuel - Solde restant\n"
+    output += "***************************************************\n"
+    output += echeancier
+    output += "***************************************************\n"
+    output += "Fait le:              à              ;\n"
+    output += "\n"
+    output += "Signatures:\n"
+    output += "\n"
+    output += "\n"
+    output += "\n"
+    output += "\n"
+    output += "***************************************************\n"
+    output
+  end
+
   def echeancier
     solde_restant = @montant
     date_paiement = @date_debut
-
-    File.open('echeancier.txt', 'w') do |f|
-        @duree.times do |i|
-            paiement = paiement_mensuel
-            solde_restant -= paiement
-            # format for cmdline display
-            date_format = "#{date_paiement.day} #{MOIS[date_paiement.month-1]} #{date_paiement.year}"
-            paiement_format = "#{paiement.round(2)}€"
-            solde_restant_format = "#{solde_restant.round(2)}€"
-            f.puts "#{date_format.center(18)} #{paiement_format.center(18)} #{solde_restant_format.center(14)}"
-            date_paiement = date_paiement >> 1 # ajouter un mois à la date
-        end
+    echeancier_text = ""
+    @duree.times do |i|
+      paiement = paiement_mensuel
+      solde_restant -= paiement
+      # format for cmdline display
+      date_format = "#{date_paiement.day} #{MOIS[date_paiement.month-1]} #{date_paiement.year}"
+      paiement_format = "#{paiement.round(2)}€"
+      solde_restant_format = "#{solde_restant.round(2)}€"
+      echeancier_text += "#{date_format.center(18)} #{paiement_format.center(18)} #{solde_restant_format.center(14)}\n"
+      date_paiement = date_paiement >> 1 # ajouter un mois à la date
     end
+    echeancier_text
   end
 end
+
 
 
 puts "Entrer le montant du prêt:"
@@ -44,24 +67,6 @@ date_debut = gets.chomp
 
 emprunt = Emprunt.new(montant, duree, date_debut)
 
-STDOUT.reopen(File.open('echeancier.txt', 'w'))
-
-puts ""
-puts "***************************************************"
-puts "Date: #{date_debut}"
-puts "Montant du prêt: #{montant}"
-puts "Durée du prêt: #{duree} ans"
-puts "Paiement mensuel: #{emprunt.paiement_mensuel.round(2)}€"
-puts "***************************************************"
-puts "Date du paiement - Paiement mensuel - Solde restant"
-puts "***************************************************"
-emprunt.echeancier
-puts "***************************************************"
-puts "Fait le:              à              ;"
-puts ""
-puts "Signatures:"
-puts ""
-puts ""
-puts ""
-puts ""
-puts "***************************************************"
+File.open('echeancier.txt', 'w') do |f|
+    f.write(emprunt.generate_output)
+end
