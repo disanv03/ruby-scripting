3d9f34b24e74426ce3f0a2486c486c32cd6c06e8
disanv03 <heddisanv@gmail.com>
2023-08-01 12:14:49 +0200

    1.5

--- basics/discovery/cookbook/recipe1-5.rb
+++ basics/discovery/cookbook/recipe1-5.rb
@@ -13,3 +13,19 @@ hexadecimal.each_byte { |x| puts x }
 # 1
 # 16
 # 32
+
+
+open('smiley.html', 'wb') do |f| # wb for binary writing
+  f << '<meta http-equiv="Content-Type" content="text/html;charset=UTF-8">'
+  f << "\xe2\x98\xBA"
+  f << "\x07"
+end
+
+puts "foo\tbar"
+puts %{foo\tbar}
+puts %Q{foo\tbar}
+
+puts 'foo\tbar'
+puts %q{foo\tbar}
+
+
