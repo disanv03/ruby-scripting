14c8bb368e7767d2061887f63b4cc2648e0b6a1d
disanv03 <heddisanv@gmail.com>
2023-08-02 10:04:08 +0200

    1.6

---
+++ basics/discovery/cookbook/recipe1-6.rb
@@ -0,0 +1,15 @@
+# Converting Between Characters and Values
+
+# Use .ord for ordinal instead of ?
+?a
+# => 97
+?! 
+# => 33
+?\n 
+# => 10
+
+a = 'choose a characters'[7]
+puts "#{a.ord}"
+
+puts "#{97.chr}"
+puts "#{10.chr.inspect}"
