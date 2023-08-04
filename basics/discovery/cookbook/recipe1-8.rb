6582884c792cda5d6d3c039e4f9ad9ed74b2ad11
disanv03 <heddisanv@gmail.com>
2023-08-04 10:51:53 +0200

    1.8

--- basics/discovery/cookbook/recipe1-8.rb
+++ basics/discovery/cookbook/recipe1-8.rb
@@ -2,3 +2,11 @@
 
 'foobar'.each_byte { |x| puts "#{x} = #{x.chr}" }
 
+# To yield each character of a string as a new one
+
+'foobar'.scan(/./) { |c| puts c }
+
+
+# String#each is used to split a string on a given separator (by default the newline)
+
+"foo\nbar".each_line { |x| puts x }
