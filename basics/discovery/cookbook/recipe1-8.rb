5f3d882f0d2a6bbb912d086598bfb327026bef8f
disanv03 <heddisanv@gmail.com>
2023-08-09 14:34:57 +0000

    formating comments

--- basics/discovery/cookbook/recipe1-8.rb
+++ basics/discovery/cookbook/recipe1-8.rb
@@ -1,8 +1,8 @@
-# Processing a String One character at a Time
+#Processing a String One character at a Time
 'foobar'.each_byte { |x| puts "#{x} = #{x.chr}" }
 
-# To yield each character of a string as a new one
+#To yield each character of a string as a new one
 'foobar'.scan(/./) { |c| puts c }
 
-# String#each is used to split a string on a given separator (by default the newline)
+#String#each is used to split a string on a given separator (by default the newline)
 "foo\nbar".each_line { |x| puts x }
