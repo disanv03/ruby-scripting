ad478cb9c02dd76c0acf12618faf5e3e83aa30a1
disanv03 <heddisanv@gmail.com>
2023-08-13 12:50:03 +0200

    1.13 & 1.14

--- basics/discovery/cookbook/recipe1-13.rb
+++ basics/discovery/cookbook/recipe1-13.rb
@@ -1,1 +1,22 @@
 # Getting the parts of a string you want
+
+s = 'My kingdom for a string!'
+
+puts s.slice(3,7)
+puts s[3,7]
+puts s[0,3]
+puts s[11,5]
+puts s[11,17]
+
+puts s[/.ing/]
+# use * for matching the preceding element zero or more times
+puts s[/str.*/]
+
+# count from the end of the string, use negative indexes
+
+puts s.slice(-7,3)
+puts s[-7,6]
+
+# getting the rightmost portion of a string
+
+puts s[15...s.length]
---
+++ basics/discovery/cookbook/recipe1-14.rb
@@ -0,0 +1,1 @@
+# Handling International Encodings
