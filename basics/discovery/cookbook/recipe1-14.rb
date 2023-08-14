8a75b8f9a839b71d8c185c32ecb8e08c5570f571
disanv03 <heddisanv@gmail.com>
2023-08-14 12:09:47 +0200

    1.14

--- basics/discovery/cookbook/recipe1-14.rb
+++ basics/discovery/cookbook/recipe1-14.rb
@@ -1,1 +1,17 @@
 # Handling International Encodings
+
+string = "\xef\xbc\xa1" + "\xef\xbc\xa2" + "\xef\xbc\xa3" + "\xef\xbc\xa4" + "\xef\xbc\xa5" + "\xef\xbc\xa6"
+
+puts string.bytesize
+puts string.size
+
+# size return the number of charaters in the string
+# bytesize return the number of byte used to represent the string in memory
+
+# ñ exemple (U+00F1)
+# In hexadecimal 0xC3, 0xB1
+n = "\xC3\xB1"
+
+puts n.bytesize
+puts n.size
+
