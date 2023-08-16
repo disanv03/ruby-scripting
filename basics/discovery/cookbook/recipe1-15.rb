a8c3524779af0b2d845f3b5fbea05cebac545e8f
disanv03 <heddisanv@gmail.com>
2023-08-16 11:03:38 +0200

    1.15

--- basics/discovery/cookbook/recipe1-15.rb
+++ basics/discovery/cookbook/recipe1-15.rb
@@ -1,1 +1,12 @@
 # Word-Wrapping Lines of Text
+
+# capture one or more whitespace characters \s+ or the end of string \Z
+def wrap(s, width=78)
+  s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
+end
+
+puts wrap("This text is too short to be wrapped.")
+
+puts wrap("This text is not too short to be wrapped.", 20)
+
+puts wrap("These ten-characters columns are stifling my creativity!", 10)
