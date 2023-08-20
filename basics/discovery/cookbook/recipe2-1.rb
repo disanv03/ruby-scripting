032299f39692a31b6c1486b00ebf9ee030b733e1
disanv03 <heddisanv@gmail.com>
2023-08-20 21:00:20 +0200

    2.1

---
+++ basics/discovery/cookbook/recipe2-1.rb
@@ -0,0 +1,30 @@
+# Parsing a Number from a String
+
+'400'.to_i
+'3.14'.to_f
+'1.602e-19'.to_f
+
+'405'.oct
+'405'.to_i(8)
+'405'.hex
+'405'.to_i(16)
+'fed'.hex
+'fed'.to_i(16)
+
+"13: a baker's dozen".to_i
+'1001 Nights'.to_i
+'The 1000 Nights and a Night'.to_i
+'60.50 Misc. Agricultural Equipment'.to_f
+'$60.50'.to_f
+'Feed the monster!'.hex
+'I fed the monster at Canoga Park Waterslides'.hex
+'0xA2Z'.hex
+'-10'.oct
+'-109'.oct
+'3.14'.to_i
+
+Integer('1001')
+Integer('1001 nights')
+Float('99.44')
+Float('99.44% pure')
+
