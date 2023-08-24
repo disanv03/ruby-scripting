46595a780373636041ec4b0d93dbfe84137f0168
disanv03 <heddisanv@gmail.com>
2023-08-24 10:17:33 +0200

    2.6

--- basics/discovery/cookbook/recipe2-6.rb
+++ basics/discovery/cookbook/recipe2-6.rb
@@ -1,1 +1,23 @@
 # Converting Between Numeric Bases
+
+puts 0b100 # => binary
+puts 0o100 # => octal
+puts 0x100 # => hexadecimal
+
+puts "1045".to_i(10)
+puts "-1001001".to_i(2)
+puts "abc".to_i(16)
+puts "abc".to_i(20)
+puts "number".to_i(36)
+puts "zz1z".to_i(36)
+puts "abcdef".to_i(16)
+puts "AbCdEf".to_i(16)
+
+puts 42.to_s(10)
+puts -100.to_s(2)
+puts 255.to_s(16)
+puts 1442151747.to_s(36)
+
+
+
+
