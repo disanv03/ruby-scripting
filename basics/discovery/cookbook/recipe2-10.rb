a2f638c1aeb6543df372fcfb0095fc80bfcf1f95
disanv03 <heddisanv@gmail.com>
2023-08-29 10:43:22 +0200

    2.11

--- basics/discovery/cookbook/recipe2-10.rb
+++ basics/discovery/cookbook/recipe2-10.rb
@@ -2,3 +2,35 @@
 #
 # Problem: You want to turn arrays of arrays of numbers into mathematical matrices,
 # and multiply the matrices together.
+
+require 'matrix'
+
+a1 = [[1,1,0,1],
+      [2,0,1,2],
+      [3,1,1,2]]
+m1 = Matrix[*a1]
+puts m1
+
+a2 = [[1,0],
+      [3,1],
+      [1,0],
+      [2,2.5]]
+m2 = Matrix[*a2]
+puts m2
+
+puts m1 * m2
+
+# Here how the multiplication work
+#
+# m1 = | 1 1 0 1 |       m2 = | 1   0 |
+#      | 2 0 1 2 |            | 3   1 |
+#      | 3 1 1 2 |            | 1   0 |
+#                             | 2 2.5 |
+
+# m1 * m2 = | (1*1 + 1*3 + 0*1 + 1*2) (1*0 + 1*1 + 0*0 + 1*2.5) |
+#           | (2*1 + 0*3 + 1*1 + 2*2) (2*0 + 0*1 + 1*0 + 2*2.5) |
+#           | (3*1 + 1*3 + 1*1 + 2*2) (3*0 + 1*1 + 1*0 + 2*2.5) |
+
+#         = | 6 2.5 |
+#           | 6 5   |
+#           | 9 5   |
---
+++ basics/discovery/cookbook/recipe2-11.rb
@@ -0,0 +1,7 @@
+# Solving a System of Linear Equations
+# = Equation that produces a straight line when graphed
+
+2x + 10y + 8z = 54
+7y + 4z = 30
+5x + 5y + 5z = 35
+
