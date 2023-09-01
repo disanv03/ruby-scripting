622036b8f2a814c006a92c375d50217b928a53f0
disanv03 <heddisanv@gmail.com>
2023-09-01 11:42:11 +0200

    2.15

--- basics/discovery/cookbook/chap2/recipe2-15.rb
+++ basics/discovery/cookbook/chap2/recipe2-15.rb
@@ -2,3 +2,41 @@
 # Problem:
 # You want to iterate over a (possible infinite) sequence of numbers the way you can
 # iterate over an array or a range
+
+def fibonacci(limit = nil)
+  seed1 = 0
+  seed2 = 1
+  while not limit or seed2 <= limit
+    yield seed2
+    seed1, seed2 = seed2, seed1 + seed2
+  end
+end
+
+fibonacci(3)  { |x| puts x }
+
+
+class Range
+  def each_slow
+    x = self.begin
+    while x <= self.end
+      yield x
+      x = x.succ
+    end
+  end
+end
+
+(1..3).each_slow { |x| puts x }
+
+class Integer
+  def double_upto(stop)
+    x = self
+    until x > stop
+      yield x
+      x = x * 2
+    end
+  end
+end
+
+10.double_upto(50) { |x| puts x }
+
+1.5.step(2.0, 0.25) { |x| puts x }
