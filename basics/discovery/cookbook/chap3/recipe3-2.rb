7920eae77b149ec291378d9bc4deade2c029c150
disanv03 <heddisanv@gmail.com>
2023-09-04 11:23:18 +0200

    3.2

---
+++ basics/discovery/cookbook/chap3/recipe3-2.rb
@@ -0,0 +1,4 @@
+# Parsing Dates, Precisely or Fuzzily
+# Problem:
+# You want to transform a string describing a date or date/time into a Date Object.
+# You might not know the format of the string ahead of time.
