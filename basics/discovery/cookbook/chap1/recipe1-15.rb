8e76fc63b1e59c281e3dc7ba3c55ff983162df7b
gweno <heddisanv@gmail.com>
2023-09-27 12:19:06 +0200

    adding comment

--- basics/discovery/cookbook/chap1/recipe1-15.rb
+++ basics/discovery/cookbook/chap1/recipe1-15.rb
@@ -1,6 +1,7 @@
 # Word-Wrapping Lines of Text
 
 # capture one or more whitespace characters \s+ or the end of string \Z
+# \\1\n backreference refers to the first capture group and add a newline at his end
 def wrap(s, width=78)
   s.gsub(/(.{1,#{width}})(\s+|\Z)/, "\\1\n")
 end
