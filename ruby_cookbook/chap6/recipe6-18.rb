2d5801a97e9859cff1ba975e7cdbf9e86220d642
disanv03 <heddisanv@gmail.com>
2023-11-04 13:41:17 +0100

    6.18 deleting file

--- ruby_cookbook/chap6/recipe6-18.rb
+++ ruby_cookbook/chap6/recipe6-18.rb
@@ -14,6 +14,16 @@ p File.exist? "doomed_directory"
 FileUtils.remove_dir "doomed_directory"
 File.exist? "doomed_directory"
 
+
+# For unempty dir use remove_dir
+# Passing :secure option to rm_rf slows down deletions
+# (it has to change the permissions on the directory before deleting it)
 Dir.mkdir "/tmp/doomed_directory"
 FileUtils.rm_rf("/tmp/doomed_directory", :secure => true)
 File.exist? "/tmp/doomed_directory"
+
+# Why isn't the :secure option the default for rm_rf ?
+# Because secure deletion isn't thread-safe: i actually changes the current
+# working directory of the process.
+# You need to choose between thread safety and a possible security hole.
+
