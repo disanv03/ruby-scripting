486c68c0ed8c36c3731d272e63fd352448bd9b30
disanv03 <heddisanv@gmail.com>
2023-11-05 12:45:05 +0100

    6.20 start, finding the files you want

---
+++ ruby_cookbook/chap6/recipe6-20.rb
@@ -0,0 +1,5 @@
+# Finding the file you want
+# Problem:
+# You want to locate all the files in a directory hierarchy that match some criteria.
+# For instance, you might want to find all the empty files, all the MP3 files, or
+# all the files named "README."
