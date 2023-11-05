2235221971dc921cf0dcb245d0f460ec41f798e9
disanv03 <heddisanv@gmail.com>
2023-11-05 18:49:37 +0100

    first half 6.20

--- ruby_cookbook/chap6/recipe6-20.rb
+++ ruby_cookbook/chap6/recipe6-20.rb
@@ -3,3 +3,17 @@
 # You want to locate all the files in a directory hierarchy that match some criteria.
 # For instance, you might want to find all the empty files, all the MP3 files, or
 # all the files named "README."
+
+require 'find'
+module Find
+  def match(*paths)
+    matched = []
+    find(*paths) { |path| matched << path if yield path }
+    return matched
+  end
+  module_function :match
+end
+
+Find.match("./") { |p| File.lstat(p).size == 0 }
+Find.match("./") { |p| ext = p[-4...p.size]; ext && ext.downcase == ".mp3" }
+Find.match("./") { |p| File.split(p)[1] == "README" }
