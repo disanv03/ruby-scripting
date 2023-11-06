2d2f23b377b151e51b74fb7441f84a8fe9412a8f
disanv03 <heddisanv@gmail.com>
2023-11-06 11:22:08 +0100

    6.20 Finding the Files you want

--- ruby_cookbook/chap6/recipe6-20.rb
+++ ruby_cookbook/chap6/recipe6-20.rb
@@ -17,3 +17,46 @@ end
 Find.match("./") { |p| File.lstat(p).size == 0 }
 Find.match("./") { |p| ext = p[-4...p.size]; ext && ext.downcase == ".mp3" }
 Find.match("./") { |p| File.split(p)[1] == "README" }
+
+Find.match("./") do |p|
+  Find.prune if p == "./tmp"
+  File.split(p)[1] == "README"
+end
+
+# Find all files that start with a particular phrase
+must_start_with = "This Ruby program"
+Find.match("./") do |p|
+  if File.file? p
+    open(p) { |f| f.read(must_start_with.size) == must_start_with }
+  else
+    false
+  end
+end
+
+# Finds files that were probably left behind by emacs sessions
+def emacs_droppings(*paths)
+  Find.match(*paths) do |p|
+    (p[-1] == ?~ and p[0] != ?~) or (p[0] == ?# and p[-1] == ?#)
+  end
+end
+
+def bigger_than(bytes, *paths)
+  Find.match(*paths) { |p| File.lstat(p).size > bytes }
+end
+
+def modified_recently(seconds, *paths)
+  time = Time.now - seconds
+  Find.match(*paths) { |p| File.lstat(p).mtime > time }
+end
+
+def possibly_abandonned(*paths)
+  Find.match(*paths) { |p| f = File.lstat(p); f.mtime == f.atime }
+end
+
+##################################################
+# MEMOS:
+# 'mtime' => Modification time, last time a file's content
+# were modified.
+# 'atime' => Access Time, last time a file was accessed
+# 'lstat' => Information about the link itself (information about file, return a File::Stat object)
+##################################################
