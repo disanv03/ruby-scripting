d10b399411e8570406c01791f919bb56e196a80a
gweno <heddisanv@gmail.com>
2023-11-07 13:54:42 +0100

    Update hosts_from_masks.rb

--- scripts_utils/IPv4/hosts_from_masks.rb
+++ scripts_utils/IPv4/hosts_from_masks.rb
@@ -7,11 +7,13 @@ def mask_to_prefix(mask)
   IPAddr.new(mask).to_i.to_s(2).count("1")
 end
 
-# Function to calculate network and broadcast addresses
+# Function to calculate network, usable host addresses, and broadcast addresses
 def calculate_addresses(ip, mask)
-  network = IPAddr.new("#{ip}/#{mask_to_prefix(mask)}")
-  broadcast = network.to_range.last
-  [network.to_range.to_a[1...-1], broadcast] # Exclude network and broadcast addresses
+  ip_with_prefix = IPAddr.new("#{ip}/#{mask_to_prefix(mask)}")
+  network = ip_with_prefix.to_range.first
+  broadcast = ip_with_prefix.to_range.last
+  usable_hosts = ip_with_prefix.to_range.to_a[1...-1] # Exclude network and broadcast addresses
+  [network, usable_hosts, broadcast]
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -22,9 +24,12 @@ if __FILE__ == $PROGRAM_NAME
 
   ip_address = ARGV[0]
   subnet_mask = ARGV[1]
-  hosts, broadcast = calculate_addresses(ip_address, subnet_mask)
+  network, hosts, broadcast = calculate_addresses(ip_address, subnet_mask)
 
-  puts "Usable Host IP Addresses:"
+  puts "Network Address:"
+  puts network.to_s
+
+  puts "\nUsable Host IP Addresses:"
   hosts.each do |host|
     puts host.to_s
   end
