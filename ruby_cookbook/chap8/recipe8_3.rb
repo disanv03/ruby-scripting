ed07b849a0cdc08b62cc3493d00e27a0c83140ff
disanv03 <heddisanv@gmail.com>
2023-11-20 11:05:38 +0100

    8.3 finished

--- ruby_cookbook/chap8/recipe8_3.rb
+++ ruby_cookbook/chap8/recipe8_3.rb
@@ -22,3 +22,43 @@ end
 
 multiply_precisely(4, 5)
 # multiply_precisely(4.0, 5)
+
+def append_to_self(x)
+  unless x.respond_to? :<<
+    raise ArgumentError, "This object doesn't support the left-shift operator"
+  end
+  if x.is_a? Numeric
+    raise ArgumentError, "The left-shift operator for this object doesn't do an append."
+  end
+  x << x
+end
+
+append_to_self('abc')
+append_to_self([1, 2, 3])
+p append_to_self({1 => 2}) # => ArgumentError: This object doesn't support the left-shift operator
+# append_to_self(5) # => ArgumentError: The left-shift operator for this object doesn't do an append
+p 5 << 5
+
+# Alternative solution approximates the functionality of Java's interfaces.
+module ShiftMeansAppend
+  def <<(x)
+  end
+end
+
+class String
+  include ShiftMeansAppend
+end
+
+class Array
+  include ShiftMeansAppend
+end
+
+def append_to_self(x)
+  unless x.is_a? ShiftMeansAppend
+    raise ArgumentError, "I can't trust this object's left-shift operator"
+  end
+  x << x
+end
+
+# append_to_self 4 #=> ArgumentError: I can't trust this object's left-shigt operator
+p append_to_self '4'
