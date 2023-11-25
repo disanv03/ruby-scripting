2b55e8dda5558465f1d462413481f4090a01a30f
disanv03 <debian@DEBIAN-DESKTOP.lan>
2023-11-25 21:59:01 +0100

    8.9

---
+++ ruby_cookbook/chap8/recipe8_9.rb
@@ -0,0 +1,9 @@
+# Converting and Coercing Objects to Different types
+# Problem:
+# You have an object of one type and you want tu use it as though it were of another type
+
+p "4".to_i
+p 4.to_s
+p Time.now.to_f
+hash = { "key1" => "value1", "key2" => "value2" }.to_a
+p hash
