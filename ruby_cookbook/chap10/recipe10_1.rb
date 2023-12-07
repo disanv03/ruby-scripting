d40904cb031ed0bbc3a6e6b10a3ee608b049329e
disanv03 <heddisanv@gmail.com>
2023-12-07 20:39:32 +0100

    10.1 first half

---
+++ ruby_cookbook/chap10/recipe10_1.rb
@@ -0,0 +1,12 @@
+# Finding an Object's class and superclass
+# Problem:
+# Given a class, you want an object corresponding to its class, or to
+# the parent of its class.
+
+p 'a string'.class
+p 'a string'.class.name
+p 'a string'.class.superclass
+p String.superclass
+p String.class
+p String.class.superclass
+p 'a string'.class.new
