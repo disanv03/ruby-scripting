a7ed2677a8be1c3adef313daf019fd2b0ec19ea1
gweno <heddisanv@gmail.com>
2024-01-31 16:57:18 +0100

    Update roman_numerals_decode.rb comments

--- math/roman_numerals_decode.rb
+++ math/roman_numerals_decode.rb
@@ -32,3 +32,19 @@ puts roman_to_arabic("MDCLXVI") #=> 1666
 puts roman_to_arabic("MMVIII") #=> 2008
 puts roman_to_arabic("MCMXC") #=> 1990
 
+
+# NOTE:
+# Shifting a binary number to the left by 1 bit is equivalent to multiplying that number by 2.
+# Remember to read and add starting from the left.
+# 
+# Example: (second M in "MCMXC" => 1990)
+# Processing 'M' (1000) :
+#    digit = 1000
+#    last_digit < digit is true (100 is less than 1000)
+#    arabic = 1100 - (100 << 1) = 1100 - 200 = 900
+#    arabic = 900 + 1000 = 1900
+#    last_digit = 1000
+#
+# In summary, the algorithm effectively adds the value of each Roman numeral to a total. 
+# However, if a smaller numeral precedes a larger one, it subtracts double the value of the smaller numeral 
+# (to correct for the previous addition and apply the necessary subtraction). 
