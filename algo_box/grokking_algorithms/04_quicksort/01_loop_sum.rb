1422ddcd4326081c6b9075d347689e057719ed06
disanv03 <heddisanv@gmail.com>
2024-02-06 11:24:18 +0100

    04 chap

---
+++ algo_box/grokking_algorithms/04_quicksort/01_loop_sum.rb
@@ -0,0 +1,11 @@
+def sum(arr)
+  total = 0
+  arr.each do |x|
+    total += x
+  end
+
+  total
+end
+
+puts sum([1,2,3,4])
+
---
+++ algo_box/grokking_algorithms/04_quicksort/02_recursive_sum.rb
@@ -0,0 +1,6 @@
+def sum(list)
+  return 0 if list.empty?
+  list[0] + sum(list[1..-1])
+end
+
+
