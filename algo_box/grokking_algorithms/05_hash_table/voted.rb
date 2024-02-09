d75d621c4a4b05a1d9f98b96fd437ab88c3213c3
disanv03 <heddisanv@gmail.com>
2024-02-09 14:02:50 +0100

    @voted

--- algo_box/grokking_algorithms/05_hash_table/voted.rb
+++ algo_box/grokking_algorithms/05_hash_table/voted.rb
@@ -1,15 +1,21 @@
-voted = {}
+# in order to be accessible from within the check_voter function
+# voted has to be an instance variable (or global / class)
+# In ruby, when you're running code in the top-level context
+# (note inside any class or module definiton), you're actually executing code
+# in the context of an object called 'main', which is an instance of 'Object'.
 
-def check_voter(name, voted)
-  if voted[name]
+@voted = {}
+
+def check_voter(name)
+  if @voted[name]
     puts "kick them out"
   else
-    voted[name] = true
+    @voted[name] = true
     puts "let them vote!"
   end
 end
 
-check_voter("tom", voted)
-check_voter("mike", voted)
-check_voter("mike", voted)
+check_voter("tom")
+check_voter("mike")
+check_voter("mike")
 
