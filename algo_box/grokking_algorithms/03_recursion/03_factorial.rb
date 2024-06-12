94569b4487131c32b90e23c98e78d5d95e226c84
gweno <heddisanv@gmail.com>
2024-06-12 14:53:01 +0200

    adding comments in 03_factorial.rb

--- algo_box/grokking_algorithms/03_recursion/03_factorial.rb
+++ algo_box/grokking_algorithms/03_recursion/03_factorial.rb
@@ -1,3 +1,15 @@
+#
+# Factorial implementation in a recursive way
+#
+# A factorial, as n!, is the product of all positive integers
+# less or equal to n.
+#
+# Ex:
+# 5! = 5 x 4 x 3 x 2 x 1 = 120
+#
+# By definiton:
+# 0! = 1
+
 def fact(x)
   if x == 1
     1
