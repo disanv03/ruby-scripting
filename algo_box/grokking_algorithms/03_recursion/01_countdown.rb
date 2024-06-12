5ec6043c607e03483f0cc2cfc227b6baba28d659
gweno <heddisanv@gmail.com>
2024-06-12 15:16:11 +0200

    adding recursive explaination comments in 01_countdown.rb

--- algo_box/grokking_algorithms/03_recursion/01_countdown.rb
+++ algo_box/grokking_algorithms/03_recursion/01_countdown.rb
@@ -16,6 +16,13 @@
 #
 # The main point it's to demonstrate that action can still be done
 # on the way back to the recursive calls, from where it left off.
+#
+# Analogy to a Stack:
+#
+# - Each recursive call adds a new frame to the stack (a last in, first-out stucture)
+# - The base case is where the recursion stops adding frames and starts removing them
+# - As each call returns, it "pops" off the stack, and the function continues to execute
+#   from where it left off in the previous frame.
 
 
 def countdown(i)
